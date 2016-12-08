f9ea8a8bb2f5ed85d004345f1f24c9c7c2a6ec26
Joost Verberk <joost.verberk@gmail.com>
2016-12-08 20:23:11 +0100

    Initial commit of DropBoxPlus project

---
+++ Visual Studio/DropBoxPlus/DropBoxPlus/Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace DropBoxPlus
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.lblDropboxUsage = new System.Windows.Forms.Label();
+            this.pbDropboxUsage = new System.Windows.Forms.PictureBox();
+            this.lblNASStatusLabel = new System.Windows.Forms.Label();
+            this.lblNASStatus = new System.Windows.Forms.Label();
+            this.pbFilesMoved = new System.Windows.Forms.PictureBox();
+            this.lblFilesMoved = new System.Windows.Forms.Label();
+            this.lblCurFileLabel = new System.Windows.Forms.Label();
+            this.lblCurFile = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pbDropboxUsage)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbFilesMoved)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblDropboxUsage
+            // 
+            this.lblDropboxUsage.AutoSize = true;
+            this.lblDropboxUsage.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDropboxUsage.Location = new System.Drawing.Point(12, 13);
+            this.lblDropboxUsage.Name = "lblDropboxUsage";
+            this.lblDropboxUsage.Size = new System.Drawing.Size(116, 15);
+            this.lblDropboxUsage.TabIndex = 2;
+            this.lblDropboxUsage.Text = "Dropbox usage [0%]";
+            // 
+            // pbDropboxUsage
+            // 
+            this.pbDropboxUsage.Location = new System.Drawing.Point(12, 32);
+            this.pbDropboxUsage.Name = "pbDropboxUsage";
+            this.pbDropboxUsage.Size = new System.Drawing.Size(260, 22);
+            this.pbDropboxUsage.TabIndex = 3;
+            this.pbDropboxUsage.TabStop = false;
+            // 
+            // lblNASStatusLabel
+            // 
+            this.lblNASStatusLabel.AutoSize = true;
+            this.lblNASStatusLabel.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNASStatusLabel.Location = new System.Drawing.Point(12, 67);
+            this.lblNASStatusLabel.Name = "lblNASStatusLabel";
+            this.lblNASStatusLabel.Size = new System.Drawing.Size(68, 15);
+            this.lblNASStatusLabel.TabIndex = 4;
+            this.lblNASStatusLabel.Text = "NAS Status:";
+            // 
+            // lblNASStatus
+            // 
+            this.lblNASStatus.AutoSize = true;
+            this.lblNASStatus.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNASStatus.Location = new System.Drawing.Point(86, 67);
+            this.lblNASStatus.Name = "lblNASStatus";
+            this.lblNASStatus.Size = new System.Drawing.Size(58, 15);
+            this.lblNASStatus.TabIndex = 5;
+            this.lblNASStatus.Text = "unknown";
+            // 
+            // pbFilesMoved
+            // 
+            this.pbFilesMoved.Location = new System.Drawing.Point(12, 114);
+            this.pbFilesMoved.Name = "pbFilesMoved";
+            this.pbFilesMoved.Size = new System.Drawing.Size(260, 22);
+            this.pbFilesMoved.TabIndex = 6;
+            this.pbFilesMoved.TabStop = false;
+            // 
+            // lblFilesMoved
+            // 
+            this.lblFilesMoved.AutoSize = true;
+            this.lblFilesMoved.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFilesMoved.Location = new System.Drawing.Point(12, 96);
+            this.lblFilesMoved.Name = "lblFilesMoved";
+            this.lblFilesMoved.Size = new System.Drawing.Size(99, 15);
+            this.lblFilesMoved.TabIndex = 7;
+            this.lblFilesMoved.Text = "Files moved [0%]";
+            // 
+            // lblCurFileLabel
+            // 
+            this.lblCurFileLabel.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCurFileLabel.Location = new System.Drawing.Point(12, 150);
+            this.lblCurFileLabel.Name = "lblCurFileLabel";
+            this.lblCurFileLabel.Size = new System.Drawing.Size(260, 15);
+            this.lblCurFileLabel.TabIndex = 8;
+            this.lblCurFileLabel.Text = "Current file:";
+            // 
+            // lblCurFile
+            // 
+            this.lblCurFile.AutoSize = true;
+            this.lblCurFile.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCurFile.Location = new System.Drawing.Point(89, 150);
+            this.lblCurFile.Name = "lblCurFile";
+            this.lblCurFile.Size = new System.Drawing.Size(0, 15);
+            this.lblCurFile.TabIndex = 9;
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 181);
+            this.Controls.Add(this.lblCurFile);
+            this.Controls.Add(this.lblCurFileLabel);
+            this.Controls.Add(this.lblFilesMoved);
+            this.Controls.Add(this.pbFilesMoved);
+            this.Controls.Add(this.lblNASStatus);
+            this.Controls.Add(this.lblNASStatusLabel);
+            this.Controls.Add(this.pbDropboxUsage);
+            this.Controls.Add(this.lblDropboxUsage);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Main";
+            this.Text = "Infinite Dropbox";
+            this.Load += new System.EventHandler(this.Main_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pbDropboxUsage)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbFilesMoved)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label lblDropboxUsage;
+        private System.Windows.Forms.PictureBox pbDropboxUsage;
+        private System.Windows.Forms.Label lblNASStatusLabel;
+        private System.Windows.Forms.Label lblNASStatus;
+        private System.Windows.Forms.PictureBox pbFilesMoved;
+        private System.Windows.Forms.Label lblFilesMoved;
+        private System.Windows.Forms.Label lblCurFileLabel;
+        private System.Windows.Forms.Label lblCurFile;
+    }
+}
+
---
+++ Visual Studio/DropBoxPlus/DropBoxPlus/Form1.cs
@@ -0,0 +1,289 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Timers;
+using System.Threading;
+using System.Text.RegularExpressions;
+using System.Drawing.Imaging;
+
+namespace DropBoxPlus
+{
+    public partial class Main : Form
+    {
+        // Constants
+        const double    DROPBOX_CAPACITY_GB = 6.25;                                        // GB
+        const double    DROPBOX_CAPACITY = DROPBOX_CAPACITY_GB * 1024 * 1024 * 1024;       // Convert to bytes
+        const int       DROPBOX_WARNING = 50; // %
+        const int       DROPBOX_ALARM = 60; // %
+        const string    DROPBOX_DIR = @"C:\Users\Joost\Dropbox";
+        const string    DROPBOX_SOURCE_DIR = @"C:\Users\joost\Dropbox\Camera Uploads";
+        const string    NAS_HOSTNAME = "nas";
+        const string    NAS_DEST_DIR = @"P:\00000000 - Smartphone Joost";
+
+        // Global variables
+        // Common variables
+        double pbUnit;
+        int pbWidth, pbHeight, DropboxLevel, ToMoveCnt = 0, MovedCnt = 0, MoveProg = 0;
+        Boolean DebugMode = false;
+        // Graphics
+        Bitmap bmpDropboxUsage, bmpFilesMoved;
+        Graphics gDropboxUsage, gFilesMoved;
+        // Timers
+        System.Windows.Forms.Timer t1 = new System.Windows.Forms.Timer();
+        System.Windows.Forms.Timer t2 = new System.Windows.Forms.Timer();
+        // Regex
+        private static Regex r = new Regex(":");
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+            // Detect debug mode
+            #if DEBUG
+            DebugMode = true;
+            #endif
+
+            // Read configuration file
+            //var appSettings = ConfigurationManager.AppSettings;
+            //Console.WriteLine(appSettings["DROPBOX_CAPACITY_GB"]);
+
+            // Format the form
+            CenterToScreen();
+            Height = 135;
+
+            // 
+            // Dropbox usage
+            // 
+
+            // Initialize bar
+            pbWidth = pbDropboxUsage.Width;
+            pbHeight = pbDropboxUsage.Height;
+            pbUnit = pbWidth / 100.0;
+
+            // Create bitmaps
+            bmpDropboxUsage = new Bitmap(pbWidth, pbHeight);
+            bmpFilesMoved = new Bitmap(pbWidth, pbHeight);
+
+            // Update Dropbox Usage
+            DropboxLevel = UpdateDropboxUsage();
+
+            // 
+            // NAS status
+            // 
+            if (CheckNAS())
+            {
+                lblNASStatus.Text = "connected";
+                lblNASStatus.ForeColor = System.Drawing.Color.Black;
+            }
+            else
+            {
+                lblNASStatus.Text = "disconnected";
+                lblNASStatus.ForeColor = System.Drawing.Color.Red;
+            }
+
+            // 
+            // Files moved
+            //           
+
+            // Initialize timers
+            t1.Enabled = false;
+            t1.Interval = 1;
+            t1.Tick += new EventHandler(OnTimedEvent1);
+            t2.Enabled = false;
+            t2.Interval = 5000;
+            t2.Tick += new EventHandler(OnTimedEvent2);
+
+            // If Dropbox is too full action is required...
+            // ... but can only happen if NAS is up!
+            if (((DropboxLevel > DROPBOX_ALARM) || DebugMode) && CheckNAS())
+            {
+                // Display the progress bar
+                this.Height += 85;
+                // Compute the amount of files to move
+                DirectoryInfo d = new DirectoryInfo(DROPBOX_SOURCE_DIR);
+                ToMoveCnt += d.GetFiles("*.jpg", SearchOption.TopDirectoryOnly).Length;
+                ToMoveCnt += d.GetFiles("*.mp4", SearchOption.TopDirectoryOnly).Length;
+                ToMoveCnt += d.GetFiles("*.png", SearchOption.TopDirectoryOnly).Length;
+
+                // Initialize progress bar
+                gFilesMoved = Graphics.FromImage(bmpFilesMoved);
+                gFilesMoved.Clear(Color.Black);
+                gFilesMoved.FillRectangle(Brushes.White, new Rectangle(2, 2, pbWidth - 4, pbHeight - 4));
+                pbFilesMoved.Image = bmpFilesMoved;
+
+                // Update label
+                lblFilesMoved.Text = "Files moved [0/" + ToMoveCnt + "]";
+
+                // Start processing
+                t1.Enabled = true;
+            }
+            else
+            {   // Show info to user and quit
+                t2.Enabled = true;                
+            }
+        }
+
+        private void OnTimedEvent1(Object myObject, EventArgs myEventArgs)
+        {
+            t1.Enabled = false;
+            MoveDropboxData();
+            t1.Enabled = true;
+        }
+
+        private void OnTimedEvent2(Object myObject, EventArgs myEventArgs)
+        {
+            t2.Enabled = false; 
+            Application.Exit();
+        }
+
+        private long DirSize(DirectoryInfo d)
+        {
+            long size = 0;
+            // Add file sizes.
+            FileInfo[] fis = d.GetFiles();
+            foreach (FileInfo fi in fis)
+            {
+                size += fi.Length;
+            }
+            // Add subdirectory sizes.
+            DirectoryInfo[] dis = d.GetDirectories();
+            foreach (DirectoryInfo di in dis)
+            {
+                size += DirSize(di);
+            }
+            return size;
+        }
+
+        private int UpdateDropboxUsage()
+        {
+            // Initialize progress bar
+            gDropboxUsage = Graphics.FromImage(bmpDropboxUsage);
+            gDropboxUsage.Clear(Color.Black);
+            gDropboxUsage.FillRectangle(Brushes.White, new Rectangle(2, 2, pbWidth - 4, pbHeight - 4));
+
+            // Get Dropbox size
+            DirectoryInfo DirDropbox = new DirectoryInfo(DROPBOX_DIR);
+            DropboxLevel = (int)(DirSize(DirDropbox) / DROPBOX_CAPACITY * 100.0);
+
+            // Update label
+            lblDropboxUsage.Text = "Dropbox usage [" + DropboxLevel + "%]";
+
+            // Update progressbar
+            if (DropboxLevel > DROPBOX_ALARM)
+                gDropboxUsage.FillRectangle(Brushes.Red, new Rectangle(2, 2, (int)(DropboxLevel * pbUnit - 4), pbHeight - 4));
+            if (DropboxLevel > DROPBOX_WARNING)
+                gDropboxUsage.FillRectangle(Brushes.DarkOrange, new Rectangle(2, 2, (int)(DropboxLevel * pbUnit - 4), pbHeight - 4));
+            else
+                gDropboxUsage.FillRectangle(Brushes.CornflowerBlue, new Rectangle(2, 2, (int)(DropboxLevel * pbUnit - 4), pbHeight - 4));
+
+            pbDropboxUsage.Image = bmpDropboxUsage;
+
+            return DropboxLevel;
+        }
+
+        private bool CheckNAS()
+        {
+            Ping pingSend = new Ping();
+            PingReply pingRec = pingSend.Send(NAS_HOSTNAME);
+            if (pingRec.Status.ToString().Equals("Success"))
+                return true;
+            else
+                return false;
+        }
+
+        private void MoveDropboxData()
+        {
+            // Initialize progress bar
+            gFilesMoved = Graphics.FromImage(bmpFilesMoved);
+            gFilesMoved.Clear(Color.Black);
+            gFilesMoved.FillRectangle(Brushes.White, new Rectangle(2, 2, pbWidth - 4, pbHeight - 4));
+
+            DirectoryInfo d = new DirectoryInfo(DROPBOX_SOURCE_DIR);
+            FileInfo[] fis = d.GetFiles();
+
+            if (ToMoveCnt == MovedCnt)
+            {
+                // Show info to user and quit
+                t2.Enabled = true;
+            }
+
+            foreach (FileInfo fi in fis)
+            {
+                // Only copy photo's and movies
+                if ((fi.Extension == ".jpg") || (fi.Extension == ".mp4") || (fi.Extension == ".png"))
+                {
+                    // Update current file label
+                    lblCurFile.Text = fi.Name;
+
+                    // Extract the year the picture was taken in
+                    int year;
+                    //if ((fi.Extension == ".jpg") || (fi.Extension == ".png"))
+                    //    year = GetYearTakenFromImage(fi.FullName, fi.Name);
+                    //else
+                    year = int.Parse(fi.Name.Substring(0, 4));
+                    string destFolder = NAS_DEST_DIR + "\\" + year + "\\";
+
+                    // Move the file if it doesn't already exist
+                    if (!System.IO.File.Exists(destFolder + fi.Name))
+                    {
+                        // Check if the year folder is already present
+                        Directory.CreateDirectory(destFolder);
+                        // Move the file
+                        System.IO.File.Move(fi.FullName, destFolder + fi.Name);
+                    }
+                    else // It is already on the NAS so remove it
+                        System.IO.File.Delete(fi.FullName);
+
+                    // Update move counter
+                    MovedCnt++;
+                    MoveProg = (int)(MovedCnt * 100.0 / ToMoveCnt );
+
+                    // Update progress label
+                    lblFilesMoved.Text = "Files moved [" + MovedCnt + "/" + ToMoveCnt + "]";
+
+                    // Update moved files progress bar
+                    gFilesMoved.FillRectangle(Brushes.CornflowerBlue, new Rectangle(2, 2, (int)(MoveProg * pbUnit - 4), pbHeight - 4));
+                    pbFilesMoved.Image = bmpFilesMoved;
+
+                    // Update dropbox progress bar
+                    UpdateDropboxUsage();
+
+                    // DEBUG
+                    // ToMoveCnt--;
+                    return;
+                }
+            }
+        }
+
+        private static int GetYearTakenFromImage(string path, string filename)
+        {
+            using (FileStream fs = new FileStream(path, FileMode.Open, FileAccess.Read))
+            using (Image myImage = Image.FromStream(fs, false, false))
+            {
+                if (myImage.PropertyIdList.Any(p => p == 36867))
+                {   // Original creation date property exists
+                    PropertyItem propItem = myImage.GetPropertyItem(36867);
+                    string dateTaken = r.Replace(Encoding.UTF8.GetString(propItem.Value), "-", 2);
+                    return DateTime.Parse(dateTaken).Year;
+                }
+                else
+                {   // Get creation date from filename
+                    return int.Parse(filename.Substring(0, 4));
+                }   
+            }
+        }
+
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
